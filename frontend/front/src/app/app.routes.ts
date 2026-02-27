41612dd49ac831009da2468a1754f0125a36bd58
infinitebolas <rouinsardneven@gmail.com>
2026-02-27 19:25:05 +0100

    Création des fichiers pour les garages et initialisation du routage

--- frontend/front/src/app/app.routes.ts
+++ frontend/front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Accueil } from './accueil/accueil';
+import { GaragesList } from './garages-list/garages-list';
+import { Garage } from './garage/garage';
+export const routes: Routes = [
+    {path: '', component:Accueil},
+    {path: 'garages', component:GaragesList},
+    {path: 'garages/:id', component:Garage}
+];
