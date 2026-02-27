afb4ff849ee95f9e6f6d5e46c2110f7d0cd7f60f
infinitebolas <rouinsardneven@gmail.com>
2026-02-27 18:00:26 +0100

    Header + base de l'accueil

--- frontend/front/src/app/app.ts
+++ frontend/front/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Accueil } from './accueil/accueil';
+import { Header } from './header/header';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [Accueil, Header]
 })
 export class App {
-  protected readonly title = signal('front');
 }
